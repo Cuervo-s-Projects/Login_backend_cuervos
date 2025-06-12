65733c1a1bee303dbaf0260d91b03455a9b9ed33
David Giraldo <jesdavgirgom97@gmail.com>
2025-06-11 21:10:22 -0500

    Configuración de CORS

--- main.py
+++ main.py
@@ -10,10 +10,18 @@ app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'ef(b$7lyx4-qjio((fcy!=c*ob0+k)b+m96o6ock=tc)ot@@ht'
 
 jwt = JWTManager(app)
-CORS(app)
+CORS(app,
+    resources={
+        r"/api/*": {
+            "origins": ["http://localhost:3000", "http://127.0.1.0:3000"],
+            "methods": ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+            "allow_headers": ["Content-Type", "Authorization"],
+            }
+        }
+    )
 Swagger(app)
 
 app.register_blueprint(auth_bp, url_prefix='/api')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='127.0.0.1', port=5000, debug=True)
